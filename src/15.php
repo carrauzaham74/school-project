42da2b1735a257bdca85e7df037fff1b07401fa9
carrauzaham74 <carrauzaham74@gmail.com>
2025-03-21 06:49:06 +0000

    Add 15.php

---
+++ src/15.php
@@ -0,0 +1,12 @@
+<?php
+// Example PHP code
+
+// This is just an example of how you might write some basic PHP code.
+// You can replace this with actual code as needed.
+
+// Example: Adding a statement to output a message
+echo "This is my PHP code in school-project.";
+
+// Or, if you have a database or another data source in mind:
+// $sql = "SELECT * FROM your_table_name";
+?>
